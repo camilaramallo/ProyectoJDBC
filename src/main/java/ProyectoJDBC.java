aa163bf4ccacac200cdc9388e3a61bac27a7c4d2
camilaramallo <lagrimaazul70@gmail.com>
2022-06-02 16:33:05 -0300

    subiendo nuevo archicvo

--- src/main/java/ProyectoJDBC.java
+++ src/main/java/ProyectoJDBC.java
@@ -1,13 +1,16 @@
 import javax.management.MBeanAttributeInfo;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ProyectoJDBC {
     public static void main(String[] args) {
         try(Connection conexion = DriverManager.getConnection("jdbc:h2:~/test","sa","");
         ){
+            Statement stmt =  conexion.createStatement();
+            ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
+
+            while(resultado.next()){
+                System.out.println(resultado.getString("nombre"));
+            }
         }catch(SQLException sqlException){
             sqlException.printStackTrace();
         }
