d7d20ea18befdd250edce5ffe17a093e93432d35
camilaramallo <axeelmoore@gmail.com>
2022-06-03 11:19:06 -0300

    refactorizando codigo

--- src/main/java/ProyectoJDBC.java
+++ src/main/java/ProyectoJDBC.java
@@ -3,7 +3,8 @@ import java.sql.*;
 
 public class ProyectoJDBC {
     public static void main(String[] args) {
-        try(Connection conexion = DriverManager.getConnection("jdbc:h2:~/test","sa","");
+        final String databaseURL = "jdbc:h2:~/test";
+        try(Connection conexion = DriverManager.getConnection(databaseURL,"sa","");
         ){
             Statement stmt =  conexion.createStatement();
             ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
