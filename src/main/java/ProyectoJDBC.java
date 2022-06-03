6f6b9d5b03f0bcd9e9e7d045ec2114d676cb3a20
axelmoore1 <axeelmoore@gmail.com>
2022-06-03 10:43:48 -0300

    factorizando codigo

--- src/main/java/ProyectoJDBC.java
+++ src/main/java/ProyectoJDBC.java
@@ -2,8 +2,9 @@ import javax.management.MBeanAttributeInfo;
 import java.sql.*;
 
 public class ProyectoJDBC {
+        private final static String dataBaseURL = "jdbc:h2:~/test";
     public static void main(String[] args) {
-        try(Connection conexion = DriverManager.getConnection("jdbc:h2:~/test","sa","");
+        try(Connection conexion = DriverManager.getConnection(dataBaseURL,"sa","");
         ){
             Statement stmt =  conexion.createStatement();
             ResultSet resultado = stmt.executeQuery("SELECT * FROM productos");
